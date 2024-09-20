467281f0385b6edcd2708d5f89ecb6dcb7217e95
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-20 10:48:12 -0600

    List view: messages area

---
+++ lib/presentation/screens/chat/chat_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:yes_no_app/presentation/widgets/chat/my_message_bubble.dart';
+
+class ChatScreen extends StatelessWidget {
+  const ChatScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Padding(
+          padding: EdgeInsets.all(3.0),
+          child: CircleAvatar(
+            backgroundImage: NetworkImage('https://pm1.aminoapps.com/7930/24fed81e5c56375061c998cdfb6ac822433db40fr1-400-333v2_128.jpg'),
+
+          ),
+        ),
+        title: const Text("Monkey D. Luffy"),
+        centerTitle: false,
+      ),
+      body: _ChatView(),
+    );
+  }
+}
+
+class _ChatView extends StatelessWidget {
+  const _ChatView();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 10),
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: 100,
+                itemBuilder: (context, index) => const MyMessageBubble(),
+                
+              )
+            ),
+            Text("The one piece is real!")
+          ],
+        ),
+      ),
+    );
+  }
+}
