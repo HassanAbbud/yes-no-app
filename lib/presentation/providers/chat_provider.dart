d9a84874a3ff344a64f7754132921da817b9ddc5
Hassan Abbud <hassanabbud@hotmail.com>
2024-09-20 18:05:07 -0600

    Http request get yes no api

--- lib/presentation/providers/chat_provider.dart
+++ lib/presentation/providers/chat_provider.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/config/helpers/yes_no_answer.dart';
 import 'package:yes_no_app/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
 
   final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
 
   List<Message> messageList = [
     Message(text: 'Hello', fromWho: FromWho.recipient),
@@ -18,7 +20,20 @@ class ChatProvider extends ChangeNotifier {
     final newMessage = Message(text: text, fromWho: FromWho.sender);
     messageList.add( newMessage );
 
+    if (text.endsWith('?')) {
+      recipientReply();
+    }
+
     notifyListeners();
+    moveScrollToBottom();
+  }
+
+  
+  Future<void> recipientReply() async {
+    final recipientMessage = await getYesNoAnswer.getAnswer();
+    messageList.add(recipientMessage);
+    notifyListeners();
+
     moveScrollToBottom();
   }
 
